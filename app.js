25e95a6190c6e9e7b02102eb1b8108084364133d
elqstux <250801860@qq.com>
2015-06-12 20:29:45 +0800

    实现对验证码的服务端验证。

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var session = require('express-session')
 
 var routes = require('./routes/index');
 var users = require('./routes/users');
@@ -19,16 +20,22 @@ app.set('view engine', 'jade');
 //app.use(favicon(__dirname + '/public/favicon.ico'));
 app.use(logger('dev'));
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({extended: false}));
 app.use(cookieParser());
+app.use(session(
+    {
+        secret: 'mySecret',
+        resave: true,
+        saveUninitialized: true
+    }));
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
 app.use('/users', users);
-app.use('/register/', register)
+app.use('/register/', register);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
     var err = new Error('Not Found');
     err.status = 404;
     next(err);
@@ -39,7 +46,7 @@ app.use(function(req, res, next) {
 // development error handler
 // will print stacktrace
 if (app.get('env') === 'development') {
-    app.use(function(err, req, res, next) {
+    app.use(function (err, req, res, next) {
         res.status(err.status || 500);
         res.render('error', {
             message: err.message,
@@ -50,7 +57,7 @@ if (app.get('env') === 'development') {
 
 // production error handler
 // no stacktraces leaked to user
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
     res.status(err.status || 500);
     res.render('error', {
         message: err.message,
--- routes/register.js
+++ routes/register.js
@@ -9,31 +9,17 @@ router.post('/getverifycode', function (req, res, next) {
         return res.json({'errMsg': 'the phone number is invalid!'});
     }
     var verifyCode = createRandom(6);
-    console.log(phoneNumber);
-    var formData =
-    {
-        'account': 'jiekou-clcs-01',
-        'pswd': 'Tch111222',
-        'mobile': '18616730721',
-        'msg': "亲爱的用户，您的手机验证码是" + verifyCode + "，此验证码半小时内有效，请尽快完成验证.",
-        'needstatus': true
-    };
 
-    request.post(
-        {
-            url: 'http://222.73.117.158/msg/HttpBatchSendSM',
-            form: formData
-        },
-        function (err, httpResponse, body) {
-            console.log("验证码发送结果: ", err, body.split('\n'));
-        });
+    sendVerifyCode(phoneNumber, verifyCode);
+
     User.findOneAndUpdate(
         {
             'phone': phoneNumber
         },
         {
             'phone': phoneNumber,
-            'verifyCode': verifyCode
+            'verifyCode': verifyCode,
+            'createTime': Date.now()
         },
         {
             'upsert': true
@@ -42,12 +28,41 @@ router.post('/getverifycode', function (req, res, next) {
             if (err) {
                 return res.json({'errMsg': 'generate mobile verify code failed!'});
             } else {
-                console.log(user);
                 return res.json(user);
             }
         });
 
 });
+
+router.get('/verify', function (req, res) {
+    var phoneNumber = req.query['phone'];
+    var verifyCode = req.query['code'];
+
+    if (phoneNumber && verifyCode) {
+        User.findOne({'phone': phoneNumber}, function (err, user) {
+
+            if (user && user.verifyCode == verifyCode) {
+
+                if (Date.now() - user.createTime > 60 * 1000) { //1分钟
+                    return res.json({'res': false, 'errMsg': '验证码过期.'});
+                } else {
+                    return res.json({'res': true});
+                }
+
+            } else if (!user) {
+                return res.json({'res': false, 'errMsg': '请输入正确的手机号码.'});
+            } else {
+                return res.json({'res': false, 'errMsg': "验证码有误，请输入正确的验证码."})
+            }
+        });
+    } else {
+        return res.json({'res': false, 'errMsg': '请输入正确的手机号码或验证码.'});
+    }
+});
+router.post('/reg', function (req, res) {
+
+});
+
 
 module.exports = router;
 
@@ -57,4 +72,22 @@ function createRandom(length) {
         result += Math.floor((Math.random() * 10))
     }
     return result;
+}
+
+function sendVerifyCode(phoneNumber, verifyCode) {
+    var formData = {
+        'account': 'jiekou-clcs-01',
+        'pswd': 'Tch111222',
+        'mobile': '' + phoneNumber,
+        'msg': "亲爱的用户，您的手机验证码是" + verifyCode + "，此验证码半小时内有效，请尽快完成验证.",
+        'needstatus': true
+    };
+
+    request.post({
+            url: 'http://222.73.117.158/msg/HttpBatchSendSM',
+            form: formData
+        },
+        function (err, httpResponse, body) {
+            console.log("验证码发送结果: ", err, body.split('\n'));
+        });
 }
